4bab516d892a8ab76f1b8e9e30e3351b9ead1518
Md Zaki Hussain <mdzakihusain@gmail.com>
2023-09-09 19:08:16 +0530

    clean code without comments

--- Searching/binarySearch.cpp
+++ Searching/binarySearch.cpp
@@ -1,20 +1,20 @@
 int binarySearch(int arr[], int size, int key){
     int start = 0;
     int end = size - 1;
-    int mid = start + (end - start)/2; //adding start + end may go out of INT_MAX
+    int mid = start + (end - start)/2; 
   
     while(start<=end){
-        //return index when found
+       
         if(arr[mid]==key)
         return mid;
-        //goto right part
+        
         else if(key > arr[mid])
         start = mid + 1;
-        //goto left part
+        
         else
         end = mid - 1;
         
-        //update mid
+        
         mid = start + (end - start)/2;
     }
     return -1;
